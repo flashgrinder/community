5792798da9e1327f25f7de395946d054a3b4de8d
flashgrinder <flashgrinder92@gmail.com>
2021-09-30 19:31:53 +0400

    make single product page

---
+++ src/libs/modules/aboutProductSlider.js
@@ -0,0 +1,29 @@
+import Swiper, { Thumbs, Zoom, EffectFade } from 'swiper';
+
+
+function init() {
+
+    Swiper.use([Thumbs, Zoom, EffectFade]);
+
+    const aboutProductSliderThumb = new Swiper(".about-product__swiper-thumb", {
+        slidesPerView: 4,
+        spaceBetween: 25,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+    });
+
+    const aboutProductSliderFull = new Swiper(".about-product__swiper-full", {
+        loop: true,
+        zoom: true,
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
+        thumbs: {
+            swiper: aboutProductSliderThumb
+        }
+    });
+
+}
+
+export default { init }
--- src/libs/scripts.js
+++ src/libs/scripts.js
@@ -1,7 +1,9 @@
 import productSlider from '../libs/modules/productSlider.js';
+import aboutProductSlider from '../libs/modules/aboutProductSlider.js';
 
 document.addEventListener('DOMContentLoaded', function(e) {
 
     productSlider.init();
+    aboutProductSlider.init();
     
 });
