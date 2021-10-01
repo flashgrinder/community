00c2530729711703580407a57ab5bea752db7169
flashgrinder <flashgrinder92@gmail.com>
2021-10-01 16:11:47 +0400

    fix header/footer, add modal

--- src/libs/scripts.js
+++ src/libs/scripts.js
@@ -1,8 +1,10 @@
 import productSlider from '../libs/modules/productSlider.js';
 import aboutProductSlider from '../libs/modules/aboutProductSlider.js';
+import modal from '../libs/modules/modal.js';
 
 document.addEventListener('DOMContentLoaded', function(e) {
-
+    
+    modal.init();
     productSlider.init();
     aboutProductSlider.init();
     
