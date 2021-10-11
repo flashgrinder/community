d943c0720b01e1bd8a9af558bb30dc6071bc76d9
flashgrinder <flashgrinder92@gmail.com>
2021-10-11 14:21:51 +0400

    make adaptive header/menu

--- src/libs/scripts.js
+++ src/libs/scripts.js
@@ -1,4 +1,5 @@
 import cursor from '../libs/modules/cursor.js';
+import burgerMenu from '../libs/modules/burger-menu.js';
 import mainScreenSlider from '../libs/modules/mainScreenSlider.js';
 import productSlider from '../libs/modules/productSlider.js';
 import newsSlider from '../libs/modules/newsSlider.js';
@@ -8,6 +9,7 @@ import modal from '../libs/modules/modal.js';
 document.addEventListener('DOMContentLoaded', function(e) {
     
     cursor.init();
+    burgerMenu.init();
     modal.init();
     mainScreenSlider.init();
     productSlider.init();
