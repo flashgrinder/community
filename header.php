300e9632b02bf0586c0a1db80db54f1bdd2c183e
flashgrinder <flashgrinder92@gmail.com>
2021-10-13 12:18:04 +0400

    remove class/fix

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
     <?php wp_head(); ?>
-<body <?php body_class('page bg--dark'); ?>>
+<body <?php body_class('page'); ?>>
     
     <!-- Modal-form -->
     <div class="modal js-modal" data-modal="modal-form">
