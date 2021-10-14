7b090f3111bfac751d0a362de9505ff8f2b76172
flashgrinder <flashgrinder92@gmail.com>
2021-10-14 17:20:39 +0400

    make single artist page

--- single-artists.php
+++ single-artists.php
@@ -3,38 +3,27 @@
 <!-- About-author -->
 <section class="about-author bg-white">
     <div class="about-author__body container">
+        <?php the_post(); ?>
         <div class="about-author__content">
             <div class="about-author__photo gs-reveal gs-reveal--from-left">
                 <img src="<?php echo STANDART_DIR; ?><?php echo STANDART_DIR; ?>img/upload/about-author-1.jpg" alt="" class="about-author__img">
+                <?php
+                    $default_attr = [
+                        'class'	=> "about-author__img",
+                        'alt'   => get_the_title()
+                    ];
+                    
+                    echo get_the_post_thumbnail( $post->ID, 'full', $default_attr ) ?>
             </div>
             <div class="about-author__description">
                 <h2 class="about-author__name title title--medium title--black-low title--w-black gs-reveal gs-reveal--from-right">
-                    Екатерина Романова
+                    <?php the_title(); ?>
                 </h2>
                 <h3 class="about-author__headline title title--small title--black-low title--w-normal gs-reveal gs-reveal--from-right">
                     Об авторе
                 </h3>
                 <div class="about-author__text wysiwyg gs-reveal gs-reveal--from-right">
-                    <p>
-                        Романова Екатерина (родилась в 1998г.) - художник, графический дизайнер.Неоднократный лауреат и победитель международных и общероссийских конкурсов в сфере живописи и графики.
-                    </p>
-                    <p>
-                        2006-2012г.- училась в художественной школе,затем,в 2012г. перешла в <a href="">Академический художественный лицей им.Б.В.Иогансона</a> при Институте им.И.Е.Репина и окончила его в 2017г. по специальности "живопись".
-                    </p>
-                    <ul>
-                        <li>
-                            2016г.- участвовала в двух выставках в Русской Христианской Гуманитарной академии, одна из них персональная.
-                        </li>
-                        <li>
-                            2017г.- поступила в институт им.И.Е.Репина на факультет графики.
-                        </li>
-                        <li>
-                            2018г. - совместно с молодыми художниками из России и Японии принимала участие в выставке «Преодоление» в рамках масштабного комплексного проекта «Такеда.Боль и Воля»,основная цель которого - способствовать развитию паллиативной помощи и новых технологий в медицине.Выставка проходила в ЦВЗ «Манеж» в Санкт-Петербурге.
-                        </li>
-                        <li>
-                            2019г. - принимала участие в выставке «Двуликий Янус» (СССР/ГДР).На выставке были представлены работы студентов Академии Художеств Дрездена и Санкт-Петербургской Академии Художеств 1950-1980-х годов,а также работы нынешних студентов.При поддержке Русских Сезонов, в рамках Санкт-Петербургского международного культурного форума.
-                        </li>
-                    </ul>
+                    <?php the_content(); ?>
                 </div>
             </div>
         </div>
@@ -54,102 +43,46 @@
         </div>
         <div class="product-slider__swiper swiper-container">
             <div class="product-slider__swiper-wrapper swiper-wrapper gs-reveal gs-reveal--from-right">
-                <div class="product-slider__swiper-slide swiper-slide">
-                    <a href="#" class="product-card product-card--slider">
-                        <div class="product-card__picture">
-                            <img src="<?php echo STANDART_DIR; ?><?php echo STANDART_DIR; ?>img/upload/product-photo-1.jpg" alt="" class="product-card__photo">
-                        </div>
-                        <div class="product-card__name title title--medium title--black-low title--w-black">
-                            Стирка
-                        </div>
-                        <div class="product-card__author title title--small title--black-low title--w-normal">
-                            Алла Рой
-                        </div>
-                        <div class="product-card__price title title--medium title--black-low title--w-black">
-                            25 000 ₽
-                        </div>
-                    </a>
-                </div>
-                <div class="product-slider__swiper-slide swiper-slide">
-                    <a href="#" class="product-card product-card--slider">
-                        <div class="product-card__picture">
-                            <img src="<?php echo STANDART_DIR; ?><?php echo STANDART_DIR; ?>img/upload/product-photo-2.jpg" alt="" class="product-card__photo">
-                        </div>
-                        <div class="product-card__name title title--medium title--black-low title--w-black">
-                            Гордыня
-                        </div>
-                        <div class="product-card__author title title--small title--black-low title--w-normal">
-                            Анисия Теплякова
-                        </div>
-                        <div class="product-card__price title title--medium title--black-low title--w-black">
-                            16 000 ₽
-                        </div>
-                    </a>
-                </div>
-                <div class="product-slider__swiper-slide swiper-slide">
-                    <a href="#" class="product-card product-card--slider">
-                        <div class="product-card__picture">
-                            <img src="<?php echo STANDART_DIR; ?><?php echo STANDART_DIR; ?>img/upload/product-photo-3.jpg" alt="" class="product-card__photo">
-                        </div>
-                        <div class="product-card__name title title--medium title--black-low title--w-black">
-                            LEBEDEV 1
-                        </div>
-                        <div class="product-card__author title title--small title--black-low title--w-normal">
-                            Екатерина Романова
-                        </div>
-                        <div class="product-card__price title title--medium title--black-low title--w-black">
-                            999 999 999 ₽
-                        </div>
-                    </a>
-                </div>
-                <div class="product-slider__swiper-slide swiper-slide">
-                    <a href="#" class="product-card product-card--slider">
-                        <div class="product-card__picture">
-                            <img src="<?php echo STANDART_DIR; ?><?php echo STANDART_DIR; ?>img/upload/product-photo-4.jpg" alt="" class="product-card__photo">
-                        </div>
-                        <div class="product-card__name title title--medium title--black-low title--w-black">
-                            Несчастный случай 2
-                        </div>
-                        <div class="product-card__author title title--small title--black-low title--w-normal">
-                            Алла Рой
-                        </div>
-                        <div class="product-card__price title title--medium title--black-low title--w-black">
-                            20 000 ₽
-                        </div>
-                    </a>
-                </div>
-                <div class="product-slider__swiper-slide swiper-slide">
-                    <a href="#" class="product-card product-card--slider">
-                        <div class="product-card__picture">
-                            <img src="<?php echo STANDART_DIR; ?><?php echo STANDART_DIR; ?>img/upload/product-photo-5.jpg" alt="" class="product-card__photo">
-                        </div>
-                        <div class="product-card__name title title--medium title--black-low title--w-black">
-                            Алчность
-                        </div>
-                        <div class="product-card__author title title--small title--black-low title--w-normal">
-                            Анисия Теплякова
-                        </div>
-                        <div class="product-card__price title title--medium title--black-low title--w-black">
-                            15 000 ₽
-                        </div>
-                    </a>
-                </div>
-                <div class="product-slider__swiper-slide swiper-slide">
-                    <a href="#" class="product-card product-card--slider">
-                        <div class="product-card__picture">
-                            <img src="<?php echo STANDART_DIR; ?><?php echo STANDART_DIR; ?>img/upload/product-photo-6.jpg" alt="" class="product-card__photo">
-                        </div>
-                        <div class="product-card__name title title--medium title--black-low title--w-black">
-                            Прыжок 1
-                        </div>
-                        <div class="product-card__author title title--small title--black-low title--w-normal">
-                            Алла Рой
-                        </div>
-                        <div class="product-card__price title title--medium title--black-low title--w-black">
-                            23 000 ₽
-                        </div>
-                    </a>
-                </div>
+            <?php
+
+                $args = array(
+                    'post_type' => 'paintings',
+                    'posts_per_page' => 8,
+                    'orderby'     => 'date',
+                    'order'       => 'DESC',
+                    'suppress_filters' => true
+                );
+
+                $wp_query = new WP_Query( $args );
+
+
+                if( have_posts() ) : 
+                    while( have_posts() ) : the_post(); ?>
+                    <div class="product-slider__swiper-slide swiper-slide">
+                        <a href="#" class="product-card product-card--slider">
+                            <div class="product-card__picture">
+                                <?php
+                                    $default_attr = [
+                                        'class'	=> "product-card__photo",
+                                        'alt'   => get_the_title()
+                                    ];
+                                                
+                                    echo get_the_post_thumbnail( $post->ID, 'medium', $default_attr ) ?>
+                            </div>
+                            <div class="product-card__name title title--medium title--black-low title--w-black">
+                                <?php the_title(); ?>
+                            </div>
+                            <div class="product-card__author title title--small title--black-low title--w-normal">
+                                <?php echo get_field('product-card_artist'); ?>
+                            </div>
+                            <div class="product-card__price title title--medium title--black-low title--w-black">
+                                <?php echo get_field('product-card_price'); ?> ₽
+                            </div>
+                        </a>
+                    </div>
+                    <?php endwhile; ?>
+                <?php endif; ?>
+                <?php wp_reset_postdata(); ?>
             </div>
         </div>
     </div>
