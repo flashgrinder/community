924c59ebcafa0a0b2bd621a624ad29e242d79b05
flashgrinder <flashgrinder92@gmail.com>
2021-10-19 12:24:50 +0400

    add meta tag for mobile

--- header.php
+++ header.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+    <meta name="theme-color" content="#ffffff"/>
+    <meta name="apple-mobile-web-app-status-bar-style" content="#ffffff"/>
     <?php wp_head(); ?>
 <body <?php body_class('page'); ?>>
     
